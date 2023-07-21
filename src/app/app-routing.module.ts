74030e2dd37da11818d81e6e65177769997858e7
codexshell <52836290+codexshell@users.noreply.github.com>
2023-07-22 02:42:50 +0300

    chore: add articles module and lazy load it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './feature/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'articles',
+    loadChildren: () =>
+      import('./feature/articles/articles.module').then(
+        (m) => m.ArticlesModule
+      ),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'articles',
+  },
+  {
+    path: '**',
+    redirectTo: 'articles',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { ContactModule } from './feature/contact/contact.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    ContactModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
